db2a8789e243001f06338a2b7465dd1db4d28843
yxotw <biel.hbb@gmail.com>
2024-10-12 23:02:23 -0300

    Update Calculadora da Graçinha.py

--- Python/Calculadora da Graçinha.py
+++ Python/Calculadora da Graçinha.py
@@ -8,9 +8,19 @@ def calcular():
     n1 = float(input1.get())
     n2 = float(input2.get())
     result = n1 + n2
-    print(f'Essa é a soma: {result}')
-    result1.configure(text=f'Resultado: {result}')
-
+    result1.configure(text=f'Resultado da Soma: {result}')
+    #Multiplicação da calculadora (adicionei essa graça aí)
+    n1 = float(input1.get())
+    n2 = float(input2.get())
+    multi = n1 * n2
+    result2.configure(text=f'Resultado da Multiplicação: {multi}')
+    #Divisão
+    n1 = float(input1.get())
+    n2 = float(input2.get())
+    divisao = n1 / n2
+    result3.configure(text=f'Resutlado da Divisão: {divisao}')
+    
+    
 tela = ctk.CTk()
 tela.title('Vossa tela')
 tela.geometry('1000x800')
@@ -28,8 +38,14 @@ input2.pack()
 butao2 = ctk.CTkButton(tela, text='Calcular', command=calcular)
 butao2.pack()
 
-result1 = ctk.CTkLabel(tela, text='Resultado: ')
+result1 = ctk.CTkLabel(tela, text='Resultado da Soma: ')
 result1.pack()
+
+result2 = ctk.CTkLabel(tela, text='Resultado da Multiplicação: ')
+result2.pack()
+
+result3 = ctk.CTkLabel(tela, text='Resulta da Divisão: ')
+result3.pack()
 
 butao = ctk.CTkButton(tela, text='Fechar', command=btn)
 butao.pack()
