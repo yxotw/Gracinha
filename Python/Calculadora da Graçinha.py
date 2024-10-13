e3ae5012c4a51dfb8e3736c4aaed18a6927a3f2a
Wellington Ayres <145718802+Wlayres@users.noreply.github.com>
2024-10-13 17:25:19 -0300

    Update layout

--- Python/Calculadora da Graçinha.py
+++ Python/Calculadora da Graçinha.py
@@ -1,53 +1,71 @@
+#Importações padrão wlayres
+import tkinter as tk
+from tkinter import Frame, PhotoImage, messagebox, ttk
+import mysql.connector
+from mysql.connector import Error
 import customtkinter as ctk
 from customtkinter import CTkCanvas, CTkLabel, CTkEntry, CTkButton, CTkToplevel
+from customtkinter import *
+from PIL import ImageTk, Image
+import pyglet
+import webbrowser
 
-def btn():
+def fechar_tela():
     tela.destroy()
 
 def calcular():
+    #Soma
     n1 = float(input1.get())
     n2 = float(input2.get())
     result = n1 + n2
     result1.configure(text=f'Resultado da Soma: {result}')
-    #Multiplicação da calculadora (adicionei essa graça aí)
+    
+    #Multiplicação
     n1 = float(input1.get())
     n2 = float(input2.get())
     multi = n1 * n2
     result2.configure(text=f'Resultado da Multiplicação: {multi}')
+    
     #Divisão
     n1 = float(input1.get())
     n2 = float(input2.get())
     divisao = n1 / n2
     result3.configure(text=f'Resutlado da Divisão: {divisao}')
     
-    
+#Tela    
 tela = ctk.CTk()
 tela.title('Vossa tela')
-tela.geometry('1000x800')
+tela.geometry('500x500')
 tela.resizable(False, False)
 
-texto = ctk.CTkLabel(tela, text='Calculadora da Graçinha', font=('enter', 40))
+texto = ctk.CTkLabel(tela, text='Calculadora da Graçinha', font=('Inter', 40))
 texto.pack()
 
-input1 = ctk.CTkEntry(tela)
-input1.pack()
+frame_input = CTkFrame(tela)
+frame_input.pack()
 
-input2 = ctk.CTkEntry(tela)
-input2.pack()
+input1 = ctk.CTkEntry(frame_input)
+input1.grid(row=0, column=0, padx=10, pady=10)
+
+input2 = ctk.CTkEntry(frame_input)
+input2.grid(row=0, column=1, padx=10, pady=10)
 
 butao2 = ctk.CTkButton(tela, text='Calcular', command=calcular)
 butao2.pack()
 
-result1 = ctk.CTkLabel(tela, text='Resultado da Soma: ')
+frame_result = CTkFrame(tela)
+frame_result.pack(pady=50)
+
+result1 = ctk.CTkLabel(frame_result, text='Resultado da Soma: ',font=('Inter', 20))
 result1.pack()
 
-result2 = ctk.CTkLabel(tela, text='Resultado da Multiplicação: ')
+result2 = ctk.CTkLabel(frame_result, text='Resultado da Multiplicação: ',font=('Inter', 20))
 result2.pack()
 
-result3 = ctk.CTkLabel(tela, text='Resulta da Divisão: ')
+result3 = ctk.CTkLabel(frame_result, text='Resulta da Divisão: ',font=('Inter', 20))
 result3.pack()
 
-butao = ctk.CTkButton(tela, text='Fechar', command=btn)
-butao.pack()
+butao_fechar = ctk.CTkButton(tela, text='Fechar', command=fechar_tela)
+butao_fechar.pack(pady=50)
 
 tela.mainloop()#Final do Codigo
