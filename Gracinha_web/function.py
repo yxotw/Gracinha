12f7a0de020b7ae451ec65fa3546873f6bec624e
Wellington Ayres <145718802+Wlayres@users.noreply.github.com>
2024-11-14 21:30:49 -0300

    Server deu problema , corrigir no proximo update

--- Gracinha_web/function.py
+++ Gracinha_web/function.py
@@ -1,5 +1,5 @@
 # function.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 
 # Função para criar o aplicativo Flask e adicionar rotas
@@ -18,8 +18,38 @@ def create_app():
         else:
             mensagem = "Boa noite!"
         return render_template('index.html', mensagem=mensagem)
-    
-    return app
 
-def calc_sensi():
-    pass
+    # Rota para o template fortnite.html
+    @app.route('/fortnite')
+    def fortnite():
+        return render_template('fortnite.html')
+
+    # Rota para o formulário de cálculo de sensibilidade
+    @app.route('/persa')
+    def form_persa():
+        return render_template('persa.html')
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/calc_persa', methods=['POST', 'GET'])
+def calc_persa():
+    if request.method == 'POST':
+        try:
+            # Pega os valores enviados via formulário
+            persadpi = float(request.form['persadpi'])
+            persaxy = float(request.form['persaxy'])
+
+            dpi_persa = 3580
+            persa_sensi = (dpi_persa / persadpi) * persaxy
+            
+            # Retorna o resultado para o template de resposta
+            return render_template("persa.html", persa_sensi=persa_sensi, persadpi=persadpi, persaxy=persaxy)
+        except ValueError:
+            return "Valores inválidos! Por favor, insira números válidos."
+    
+    return render_template('persa.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Gracinha_web/server.py
+++ Gracinha_web/server.py
@@ -1,8 +1,8 @@
-# server.py
 from function import create_app
 
 # Cria a aplicação Flask
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()  # Inicia o servidor Flask
+    # Habilita o modo de depuração para reiniciar automaticamente e executa o app
+    app.run(debug=True)
